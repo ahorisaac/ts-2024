170ccc95f3f28d754624d28cc120b2ac6df5da73
ahorisaac <ahorisaac.ai@gmail.com>
2024-05-02 22:10:21 +0100

    typescript: number type

---
+++ src/types/number.ts
@@ -0,0 +1,21 @@
+let myNumber: number = 42;
+
+// what we can do with the number type
+
+myNumber = 100;
+
+myNumber = 32.4;
+
+myNumber = -150;
+
+myNumber = 2 + 2;
+myNumber = 2 - 2;
+myNumber = 2 * 2;
+myNumber = Infinity * .12;
+
+myNumber = parseFloat("42");
+
+// what we cannot do with the number type
+// myNumber = "42";
+
+// myNumber.toUpperCase();
---
+++ src/types/string.ts
@@ -0,0 +1,16 @@
+let firstName: string = "Ahor";
+ 
+let fullName: string = `${firstName} Isaac`;
+
+console.log("🚀 ~ :", fullName.split(" "));
+console.log("🚀 ~ :", fullName.toUpperCase());
+
+// what we cannot do
+
+// Math.max(fullName);
+
+// Math.log(fullName);
+
+// fullName.toExponential();
+
+// fullName = 412;
