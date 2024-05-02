2f4267467207893ee16515e3531f4ebd868c2b88
ahorisaac <ahorisaac.ai@gmail.com>
2024-05-02 22:26:41 +0100

    typescript: boolean type

---
+++ src/types/boolean.ts
@@ -0,0 +1,26 @@
+let myBoolean: boolean = true;
+
+// what we can do with the boolean type
+
+myBoolean = false;
+
+myBoolean = 100 > 10;
+
+myBoolean = 100 >= 10;
+
+myBoolean = typeof 100 === "number";
+myBoolean = typeof 100 !== "number";
+myBoolean = typeof "100" === "string";
+myBoolean = typeof "trey" !== "string"
+
+// what we cannot do with the boolean type
+
+// myBoolean = null;
+
+// myBoolean = undefined;
+
+// myBoolean = "true";
+
+// myBoolean = 100;
+
+// myBoolean = {};
